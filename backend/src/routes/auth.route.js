e575006e85e226aacbc26a316b7a260bd08af559
devsantoki211 <santokidev6@gmail.com>
2026-07-12 09:39:03 +0530

    login , logout routes implement complete

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { login, logout } from '../controllers/auth.controller.js';
+import authenticate from '../middlewares/auth.middleware.js';
+
+const router = express.Router();
+router.post("/login",login);
+router.post("/logout",authenticate,logout);
+
+export default router;
