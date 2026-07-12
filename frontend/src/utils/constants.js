12031f0937231b7a53d76da69dc3197680efea28
Your Name <krishpatel00001@gmail.com>
2026-07-12 14:42:12 +0530

    constants  added in utils

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,9 @@
+export const ROLES = {
+  FLEET_MANAGER: "FLEET_MANAGER",
+  SAFETY_OFFICER: "SAFETY_OFFICER",
+  FINANCIAL_ANALYST: "FINANCIAL_ANALYST",
+  DRIVER: "DRIVER",
+};
+
+export const VEHICLE_STATUS = ["Available", "In Use", "In Shop", "Retired"];
+export const TRIP_STATUS = ["Planned", "Dispatched", "Completed", "Cancelled"];
